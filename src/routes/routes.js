5da48ce683622b3726b8f7566f89342c95727c4b
Uwem Akan Nkereuwem <uwemakan12@gmail.com>
2020-04-13 03:11:49 +0100

    added custom morgan token to format response time

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -45,7 +45,7 @@ function router() {
           res.status(404);
           return res.send('Ooops! resource not found');
         }
-        res.set('Content-Type', 'application/text');
+        res.set('Content-Type', 'text/plain');
         return res.send(data);
       });
     });
